c66265084eb295f901d740863c6cf2efa59714e7
ivan94 <ivandalcantara@gmail.com>
2015-04-07 00:27:50 -0400

    Documentation - test page and json format

---
+++ code/project/web/comunication_format.js
@@ -0,0 +1,23 @@
+{
+	'id': 11111111,
+	'sensors': [
+		{
+			'serial': 'serial0000000_1',
+			'data': [
+				{
+					'timestamp': 321321,
+					'depth': 12312,
+					'data':[5]
+			]
+		},
+		{
+			'serial': 'serial0000000_1',
+			'data': [
+				{
+					'timestamp': 321321,
+					'depth': 12312,
+					'data':[5]
+			]
+		},
+	]
+}
