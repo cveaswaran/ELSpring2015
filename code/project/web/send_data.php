f45bc6dac7ff2abb545caf42f083cbc5eff65bfd
ivan94 <ivandalcantara@gmail.com>
2015-04-07 10:03:58 -0400

    send data php

---
+++ code/project/web/send_data.php
@@ -0,0 +1,15 @@
+<?php
+$dataJSON = file_get_contents("php://input")
+
+$data = json_decode($dataJSON);
+
+$conn = new mysqli("localhost", "fernandi2", "s123194", "fernandi2_db");
+
+$sql = "SELECT alias FROM el_pi";
+
+$rs = $sql->query($sql);
+$rs->data_seek(0);
+while($row = $rs->fetch_assoc()){
+    echo $row['alias'] . '<br>';
+}
+$
